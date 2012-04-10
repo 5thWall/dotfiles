c1eb37c412f4ea7b7ab7b4a9981e9089e0d08784
Andy <5thWall@gmail.com>
2012-04-09 23:18:46 -0700

    Colors in manpages

--- dots/zsh/exports.zsh
+++ dots/zsh/exports.zsh
@@ -4,10 +4,19 @@ export LANG="en_US"
 
 # Make vim the default editor
 export EDITOR="vim"
+
 # Don’t clear the screen after quitting a manual page
 export MANPAGER="less -X"
 # Highlight section titles in manual pages
-export LESS_TERMCAP_md="%{$GREEN%}"
+# Less Colors for Man Pages
+# http://linuxtidbits.wordpress.com/2009/03/23/less-colors-for-man-pages/
+export LESS_TERMCAP_mb=$'\E[01;31m'       # begin blinking
+export LESS_TERMCAP_md=$'\E[01;38;5;74m'  # begin bold
+export LESS_TERMCAP_me=$'\E[0m'           # end mode
+export LESS_TERMCAP_se=$'\E[0m'           # end standout-mode
+export LESS_TERMCAP_so=$'\E[38;33;246m'   # begin standout-mode - info box
+export LESS_TERMCAP_ue=$'\E[0m'           # end underline
+export LESS_TERMCAP_us=$'\E[04;38;5;146m' # begin underline
 
 # Larger bash history (allow 32³ entries; default is 500)
 export HISTSIZE=32768
