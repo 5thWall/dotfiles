52484f10b9b10e44a8ed5bccd24cd9bb15bcd840
Andy <5thWall@gmail.com>
2018-02-15 21:06:49 -0700

    It seems to be working now

--- dots/zsh/completion.zsh
+++ dots/zsh/completion.zsh
@@ -1,4 +1,8 @@
 # Add tab completion for SSH hostnames based on ~/.ssh/config, ignoring wildcards
+if ! which complete > /dev/null; then
+  autoload -U bashcompinit
+  bashcompinit
+fi
 [ -e "$HOME/.ssh/config" ] && complete -o "default" -o "nospace" -W "$(grep "^Host" ~/.ssh/config | grep -v "[?*]" | cut -d " " -f2)" scp sftp ssh
 
 [ -r "$HOME/.asdf/completions/asdf.bash" ] && source $HOME/.asdf/completions/asdf.bash
